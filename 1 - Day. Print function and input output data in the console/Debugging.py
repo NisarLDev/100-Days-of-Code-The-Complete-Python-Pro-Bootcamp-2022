0102c67d315af17d9079eaed6943b758ae9846da
NisarLDev <HarusBadah@proton.me>
2022-11-30 22:06:35 +0100

    First day exercises. Debugging Print function

---
+++ 1 - Day. Print function and input output data in the console/Debugging.py
@@ -0,0 +1,20 @@
+#Original code
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.")
+#
+
+#Debugged code:
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+#END OF CODE
+
+
+
+#URL Repository: https://github.com/NisarLDev/100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-2022
+#URL exercise debugging: https://replit.com/@NisarLDev/day-1-2-exercise?v=1
+#URL Test: https://replit.com/@NisarLDev/day-1-2-test-your-code?v=1
+#END OF FILE
