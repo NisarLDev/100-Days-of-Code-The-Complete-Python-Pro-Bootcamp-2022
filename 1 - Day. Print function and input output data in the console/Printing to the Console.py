aa8b81b9b70f517f08eb91ac2e1cb4b78c2416a0
NisarLDev <HarusBadah@proton.me>
2022-11-30 20:57:19 +0100

    Print funtion

---
+++ 1 - Day. Print function and input output data in the console/Printing to the Console.py
@@ -0,0 +1,7 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#URL Repository: https://github.com/NisarLDev/100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-2022
+#URL: https://replit.com/@NisarLDev/day-1-1-exercise#main.py
+#END OF FILE
