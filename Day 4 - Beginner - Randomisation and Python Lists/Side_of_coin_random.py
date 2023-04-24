b120eb91964e2bf6e762b86ef9de078e05cfe159
NisarLDev <HarusBadah@proton.me>
2023-04-24 08:36:31 +0200

    Added two exercises. Randomisation numbers

---
+++ Day 4 - Beginner - Randomisation and Python Lists/Side_of_coin_random.py
@@ -0,0 +1,14 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random;
+
+random_side_of_coin = random.randint(0, 1);
+
+if random_side_of_coin == 1:
+  print("Heads");
+
+else:
+  print("Tails")
+
+#END OF CODE
+#END OF FILE
