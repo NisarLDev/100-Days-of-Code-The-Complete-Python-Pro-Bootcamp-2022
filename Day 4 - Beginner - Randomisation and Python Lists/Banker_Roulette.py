75b9ffc5508ab5b838223784144b843d550317d3
NisarLDev <HarusBadah@proton.me>
2023-05-02 02:14:12 +0200

    Added the solution of Angela Yu.

--- Day 4 - Beginner - Randomisation and Python Lists/Banker_Roulette.py
+++ Day 4 - Beginner - Randomisation and Python Lists/Banker_Roulette.py
@@ -1,13 +1,13 @@
 #Test programme
 
-import random
+import random;
 # 🚨 Don't change the code below 👇
-test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
+test_seed = int(input("Create a seed number: "));
+random.seed(test_seed);
 
 # Split string method
-namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
-names = namesAsCSV.split(", ")
+namesAsCSV = input("Give me everybody's names, seperated by a comma. ");
+names = namesAsCSV.split(", ");
 # 🚨 Don't change the code above 👆
 #Finish of code for the tests
 
@@ -18,17 +18,27 @@ names = namesAsCSV.split(", ")
 
 #Write your code below this line 👇
 # Using random module
-import random
+# The solution of Angela Yu
+# Get the total number of items in list.
+num_items = len(names);
+# Generate random numbers between 0 and the last index.
+random_choice = random.randint(0, num_items - 1);
+person_who_will_pay = names[random_choice];
 
-def selectRandom(names):
- return random.choice(names)
+print(person_who_will_pay + "is goin to buythemeal today!");
+
+# My chosen solution 
+import random;
+
+def selectRandom(names):;
+ return random.choice(names);
 print(selectRandom(names), "is goin to buy the meal today!");
 
 # Using secrets module for secure PRNG
-import secrets
+import secrets;
 
-def selectRandom(names):
- return secrets.choice(names)
+def selectRandom(names):;
+ return secrets.choice(names);
 print(selectRandom(names), "is goin to buy the meal today!");
 
 #END OF CODE
