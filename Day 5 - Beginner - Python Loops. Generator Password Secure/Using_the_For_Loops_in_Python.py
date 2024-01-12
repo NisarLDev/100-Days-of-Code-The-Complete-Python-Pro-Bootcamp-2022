c2fe0d60a6059dea9388a923a67198b1c2680819
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-12 23:18:21 +0100

    Created Function of Loop in Python with For Loop

---
+++ Day 5 - Beginner - Python Loops. Generator Password Secure/Using_the_For_Loops_in_Python.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+#Out of the Loop For
+#Print the Vector "fruits"
+print(fruits)
+#End of Code
+#End of File
