7d839bb540d6cd8826d21ef1104a37d4294dd5b8
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-13 02:44:42 +0100

    Finished PyPassword generator

--- Day 5 - Beginner - Python Loops. Generator Password Secure/Day._5.5_PyPassword_Generator.py
+++ Day 5 - Beginner - Python Loops. Generator Password Secure/Day._5.5_PyPassword_Generator.py
@@ -1,3 +1,56 @@
+#Angela Yu Version:
+
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+#Hard Level
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+##############################################################################################
+# Password generator secure function
+#Author Bala Priya C.
+
 # necessary imports
 import secrets
 import string
@@ -13,11 +66,11 @@ alphabet = letters + digits + special_chars
 pwd_length = 25
 
 # generate a password string
-pwd = ''
-for i in range(pwd_length):
-  pwd += ''.join(secrets.choice(alphabet))
+#pwd = ''
+#for i in range(pwd_length):
+#  pwd += ''.join(secrets.choice(alphabet))
 
-print(pwd)
+#print(pwd)
 
 # generate password meeting constraints
 while True:
