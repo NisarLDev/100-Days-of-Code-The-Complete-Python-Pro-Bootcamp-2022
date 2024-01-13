09e7d5b0a55511d613fbf3d0809a0a02b5ee5c24
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-13 22:06:40 +0100

    Created My_function to sum the length of the word printed to shell.

---
+++ Day 6 - Beginner - functions and karel/My_function.py
@@ -0,0 +1,8 @@
+def my_function():
+    print("Hello World")
+    len("Hello World")
+    print("Bye")
+    len("Bye")
+    total = len("Hello World") + len("Bye")
+    print(f"{total} This is the sum of the length of both words 'Hello World' and 'Bye'")
+my_function()
