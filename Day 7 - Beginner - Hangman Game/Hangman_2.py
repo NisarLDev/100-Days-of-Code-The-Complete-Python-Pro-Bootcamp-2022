9d9b3e948a3f2d121b2cd06cca0d2acafe566aab
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-17 08:13:42 +0100

    Update Hangman_2.py

--- Day 7 - Beginner - Hangman Game/Hangman_2.py
+++ Day 7 - Beginner - Hangman Game/Hangman_2.py
@@ -34,3 +34,5 @@ for position in range(word_length:
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+print(display)
