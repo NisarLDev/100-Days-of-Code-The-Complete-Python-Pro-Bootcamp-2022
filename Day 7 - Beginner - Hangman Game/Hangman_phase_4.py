9d456daf10fe2c17358e9b9401b0f36e10b2b238
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-17 09:05:32 +0100

    Created function to print the end of the game

--- Day 7 - Beginner - Hangman Game/Hangman_phase_4.py
+++ Day 7 - Beginner - Hangman Game/Hangman_phase_4.py
@@ -93,7 +93,8 @@ while not end_of_game:
     if guess not in chosen_word:
       lives -= 1
       if lives == 0:
-        
+        end_of_game = True
+        print("You lose.")
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
