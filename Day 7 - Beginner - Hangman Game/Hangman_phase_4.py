6476f777abd84b929e2d3c10c44616886566c37c
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-17 09:20:24 +0100

    Update Hangman_phase_4

--- Day 7 - Beginner - Hangman Game/Hangman_phase_4.py
+++ Day 7 - Beginner - Hangman Game/Hangman_phase_4.py
@@ -82,19 +82,19 @@ while not end_of_game:
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+       # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-        
 
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
-      lives -= 1
-      if lives == 0:
-        end_of_game = True
-        print("You lose.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
