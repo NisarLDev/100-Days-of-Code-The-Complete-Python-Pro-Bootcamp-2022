67f5638a80920f5b372c8eb064bbe4a1fcfde99f
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-17 08:10:49 +0100

    Update Hangman_2.py

--- Day 7 - Beginner - Hangman Game/Hangman_2.py
+++ Day 7 - Beginner - Hangman Game/Hangman_2.py
@@ -12,10 +12,11 @@ print(f'Pssst, the solution is {chosen_word}.')
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
 display = []
+word_length = len(chosen_word)
 # Is this work the same
-# for _ in range(len(chosen_word)):
+#for letter in chosen_word:
 
-for letter in chosen_word:
+for _ in range(word_length):
     display += "_"
 print(display)
 
