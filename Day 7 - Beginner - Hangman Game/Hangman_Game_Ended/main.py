edab93b4a07c29fc3ee65d0749bab0371aadff4f
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-17 09:41:48 +0100

    Importing hangman_list_of_words to main function

--- Day 7 - Beginner - Hangman Game/Hangman_Game_Ended/main.py
+++ Day 7 - Beginner - Hangman Game/Hangman_Game_Ended/main.py
@@ -1,7 +1,7 @@
 #Step 5
 
 import random
-
+import hangman_list_of_words
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
