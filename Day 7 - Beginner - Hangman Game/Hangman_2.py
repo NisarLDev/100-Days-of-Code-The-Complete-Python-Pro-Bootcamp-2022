69255927c6654b46d8890271875bdd88b7fba0af
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-17 08:12:52 +0100

    Update Hangman_2.py

--- Day 7 - Beginner - Hangman Game/Hangman_2.py
+++ Day 7 - Beginner - Hangman Game/Hangman_2.py
@@ -26,7 +26,7 @@ guess = input("Guess a letter: ").lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for position in range:
+for position in range(word_length:
   letter = chosen_word[position]
   if letter == guess:
       display[position] = letter
