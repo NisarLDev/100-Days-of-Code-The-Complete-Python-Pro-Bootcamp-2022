3c33a5e376868d8342dd34a69aec72472950798b
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-02-23 21:48:05 +0100

    Added comment

--- Day 8 - Begginer - Function Parameters and Caesar Cipher/main.py
+++ Day 8 - Begginer - Function Parameters and Caesar Cipher/main.py
@@ -2,6 +2,8 @@
 # Create a function called greet(). 
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
+#
+# Simple Function
 def greet():
 # Three print sequence
   print("First")
