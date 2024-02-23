c34d6d624f50baa55b6a636187f2ee4c55726204
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-02-23 22:05:31 +0100

    Added new comment

--- Day 8 - Begginer - Function Parameters and Caesar Cipher/main.py
+++ Day 8 - Begginer - Function Parameters and Caesar Cipher/main.py
@@ -4,17 +4,17 @@
 # Call the greet() function and run your code.
 #
 # Simple Function
-def greet():
+#def greet():
 # Three print sequence
-  print("First")
-  print("Second")
-  print("Third")
-greet()
+#  print("First")
+#  print("Second")
+#  print("Third")
+#greet()
 
 # Function that allows for input
 # Parameter named "name"
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+#def greet_with_name(name):
+#    print(f"Hello {name}")
+#    print(f"How do you do {name}?")
 # The current argument for parameter named "name" is "Python"
-greet_with_name("Python")
+#greet_with_name("Python")
