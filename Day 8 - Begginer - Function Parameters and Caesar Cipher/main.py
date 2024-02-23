dfc8c075e2d7cf99d54eccedef30ecbfb138c47d
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-02-23 22:00:42 +0100

    Added comments for explain the structure of the function

--- Day 8 - Begginer - Function Parameters and Caesar Cipher/main.py
+++ Day 8 - Begginer - Function Parameters and Caesar Cipher/main.py
@@ -12,9 +12,9 @@ def greet():
 greet()
 
 # Function that allows for input
-
+# Parameter named "name"
 def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
-
+# The current argument for parameter named "name" is "Python"
 greet_with_name("Python")
