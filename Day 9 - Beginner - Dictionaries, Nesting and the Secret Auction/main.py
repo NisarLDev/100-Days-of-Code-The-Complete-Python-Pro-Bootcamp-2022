78e4e0f03f72f687688827513b3db14a3575be45
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-23 23:28:54 +0100

    Updated main

--- Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/main.py
+++ Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/main.py
@@ -8,14 +8,21 @@ student_scores = {
 # 🚨 Don't change the code above 👆
 
 #TODO-1: Create an empty dictionary called student_grades.
-
+student_grades = {}
 
 #TODO-2: Write your code below to add the grades to student_grades.👇
+   
+student_grades ["Scores 91 - 100"] =  "Grade = Outstanding"
+student_grades ["Scores 81 - 90"] = "Grade = Exceeds Expectations"
+student_grades ["Scores 71 - 80"] = "Grade = Acceptable"
+student_grades ["Scores 70 or lower"] = "Grade = Fail"
 
-    
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
+
+
+
 
 
 
